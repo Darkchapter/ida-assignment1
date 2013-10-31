2d968e7681d45833bf32ea82551dd5358237a739
Sarah Haefele <haefele@mail.sfsu.edu>
2013-10-30 22:20:53 -0800

    calendar added

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,7 @@ class IndexController < ApplicationController
   
   def project_2
   end
+  
+  def project_3
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Assignment0::Application.routes.draw do
   
   #if the URL is pages/projects_2 call controller index and action/method projects_2, the route's name is projects2 here
   get '/pages/project_2', to: 'index#project_2', as: 'project_2'
-  
+
+  get '/calendar', to: 'index#project_3', as: 'project_3'
 end
